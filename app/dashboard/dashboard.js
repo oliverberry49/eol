e4e46ca0d377cdfda42d1baacae7a72521b61fc9
Anna Thomas <annathom@uk.ibm.com>
2016-10-12 16:21:38 +0100

    Update dashboard graph to include model csv

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -63,110 +63,133 @@ angular.module('myApp')
       date: date
     };
   }, function(error, rows) {
-    var categories = ['Overdue', '6 months', '12 months', '18 months', '24 months'];
+    if (error) {
+      console.log(error);
+      return;
+    }
 
-    var dollars = [
-      overview.overdue,
-      overview.six,
-      overview.twelve,
-      overview.eighteen,
-      overview.twentyfour
-    ];
+    d3.csv('hw_eol.csv', function(d) {
+      var dateString = d['EoL or EoS Date'];
+      var split = dateString.split('/');
+      var date = new Date(split[2], split[1], split[0]);
 
-    var max = Math.max(...dollars);
-    max = Math.ceil(max / 10) * 10;
+      if (date < now) {
+        overview.overdue++;
+      } else if (date < sixMonths) {
+        overview.six++;
+      } else if (date < twelveMonths) {
+        overview.twelve++;
+      } else if (date < eighteenMonths) {
+        overview.eighteen++;
+      } else if (date < twentyfourMonths) {
+        overview.twentyfour++;
+      }
 
-    var rect = angular.element(document.getElementById('eol-dashboard'))[0].getBoundingClientRect();
-    var canvasWidth = rect.right - rect.left - 50;
-    var canvasHeight = rect.bottom - rect.top;
-    canvasHeight = (canvasHeight > 550) ? 550 : canvasHeight;
+      return {
+        os: d.OS,
+        date: date
+      };
+    }, function(error, rows) {
+      if (error) {
+        console.log(error);
+        return;
+      }
 
-    var canvas = d3.select('#eol-graph')
-        .append('svg')
-        .attr({'width': canvasWidth,'height': canvasHeight});
+      var categories = ['Overdue', '6 months', '12 months', '18 months', '24 months'];
 
-    var margin = {top: 20, right: 20, bottom: 70, left: 100};
-    var width = canvasWidth - margin.left - margin.right;
-    var height = canvasHeight - margin.top - margin.bottom;
+      var dollars = [
+        overview.overdue,
+        overview.six,
+        overview.twelve,
+        overview.eighteen,
+        overview.twentyfour
+      ];
 
-    var colors = ['#dc3912','#ff9900','#fdae6b','#ebcd30','#98df8a'];
+      var max = Math.max(...dollars);
+      max = Math.ceil(max / 10) * 10;
 
-    var grid = d3.range(5).map(function() {
-      return {'x1': 0, 'y1': height - 10, 'x2': 0, 'y2': height};
+      var rect = angular.element(document.getElementById('eol-dashboard'))[0].getBoundingClientRect();
+      var canvasWidth = rect.right - rect.left - 50;
+      var canvasHeight = rect.bottom - rect.top;
+      canvasHeight = (canvasHeight > 550) ? 550 : canvasHeight;
+
+      var canvas = d3.select('#eol-graph')
+          .append('svg')
+          .attr({'width': canvasWidth,'height': canvasHeight});
+
+      var margin = {top: 20, right: 20, bottom: 70, left: 100};
+      var width = canvasWidth - margin.left - margin.right;
+      var height = canvasHeight - margin.top - margin.bottom;
+
+      var colors = ['#dc3912','#ff9900','#fdae6b','#ebcd30','#98df8a'];
+
+      var xscale = d3.scale.linear()
+          .domain([0, max])
+          .range([0, width]);
+
+      var yscale = d3.scale.linear()
+          .domain([0, categories.length])
+          .range([0, height]);
+
+      var colorScale = d3.scale.quantize()
+          .domain([0, categories.length])
+          .range(colors);
+
+      var xAxis = d3.svg.axis()
+          .orient('bottom')
+          .scale(xscale);
+
+      var yAxis = d3.svg.axis()
+          .orient('left')
+          .scale(yscale)
+          .tickValues(d3.range(categories.length))
+          .tickFormat(function(d, i) {
+            return categories[i];
+          });
+
+      canvas.append('g')
+          .attr("transform", "translate(" + margin.left + ", " + (height + margin.top) + ")")
+          .attr('id', 'xaxis')
+          .call(xAxis);
+
+      canvas.append('g')
+          .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+          .attr('id', 'yaxis')
+          .call(yAxis)
+        .selectAll(".tick text")
+          .attr("y", 45);
+
+      canvas.append('g')
+          .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+          .attr('id','bars')
+          .selectAll('rect')
+          .data(dollars)
+          .enter()
+          .append('rect')
+          .attr('height', 70)
+          .attr({
+            'x': 0,
+            'y': function(d, i) { return yscale(i) + 10; }
+          })
+          .style('fill', function(d, i) { return colorScale(i); })
+          .attr('width', function() { return 0; })
+          .on("click", function() {
+            $state.go('home.table');
+          })
+          .on('mouseover', function(d, i) {
+            d3.select(this).style("fill", colourLuminance(colorScale(i), 0.2));
+          })
+          .on('mouseout', function(d, i) {
+            d3.select(this).style("fill", colorScale(i));
+          });
+
+      d3.select("svg").selectAll("rect")
+          .data(dollars)
+          .transition()
+          .duration(1000)
+          .attr("width", function(d) {
+            return xscale(d);
+          });
     });
-
-    var tickVals = grid.map(function(d, i) {
-      return i * 5;
-    });
-
-    var xscale = d3.scale.linear()
-        .domain([0, max])
-        .range([0, width]);
-
-    var yscale = d3.scale.linear()
-        .domain([0, categories.length])
-        .range([0, height]);
-
-    var colorScale = d3.scale.quantize()
-        .domain([0, categories.length])
-        .range(colors);
-
-    var xAxis = d3.svg.axis()
-        .orient('bottom')
-        .scale(xscale)
-        .tickValues(tickVals);
-
-    var yAxis = d3.svg.axis()
-        .orient('left')
-        .scale(yscale)
-        .tickValues(d3.range(categories.length))
-        .tickFormat(function(d, i) {
-          return categories[i];
-        });
-
-    canvas.append('g')
-        .attr("transform", "translate(" + margin.left + ", " + (height + margin.top) + ")")
-        .attr('id', 'xaxis')
-        .call(xAxis);
-
-    canvas.append('g')
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-        .attr('id', 'yaxis')
-        .call(yAxis)
-      .selectAll(".tick text")
-        .attr("y", 45);
-
-    canvas.append('g')
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
-        .attr('id','bars')
-        .selectAll('rect')
-        .data(dollars)
-        .enter()
-        .append('rect')
-        .attr('height', 70)
-        .attr({
-          'x': 0,
-          'y': function(d, i) { return yscale(i) + 10; }
-        })
-        .style('fill', function(d, i) { return colorScale(i); })
-        .attr('width', function() { return 0; })
-        .on("click", function() {
-          $state.go('home.table');
-        })
-        .on('mouseover', function(d, i) {
-          d3.select(this).style("fill", colourLuminance(colorScale(i), 0.2));
-        })
-        .on('mouseout', function(d, i) {
-          d3.select(this).style("fill", colorScale(i));
-        });
-
-    d3.select("svg").selectAll("rect")
-        .data(dollars)
-        .transition()
-        .duration(1000)
-        .attr("width", function(d) {
-          return xscale(d);
-        });
   });
-
 }]);
